9804c79f445cf5fa2845c2187b497687503bf815
Juan Lopes <juanfigarolopes@gmail.com>
2022-05-27 23:47:07 -0300

    Fixing Categories sintaxe + Migrations

--- src/Infra/Data/ApplicationDbContext.cs
+++ src/Infra/Data/ApplicationDbContext.cs
@@ -7,7 +7,7 @@ public class ApplicationDbContext : DbContext
 {
 
     public DbSet<Product> Products { get; set; }
-    public DbSet<Category> Category { get; set; }
+    public DbSet<Category> Categories { get; set; }
 
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
