ed9af88976b77524a51b1fe1168a33a4308d41b1
Juan Lopes <juanfigarolopes@gmail.com>
2022-05-28 22:23:32 -0300

    Creating Flunt validaton

--- src/Endpoints/Categories/CategoryPost.cs
+++ src/Endpoints/Categories/CategoryPost.cs
@@ -11,10 +11,13 @@ public class CategoryPost
 
     public static IResult Action(CategoryRequest categoryRequest, ApplicationDbContext context)
     {
-        var category = new Category
+        var category = new Category(categoryRequest.Name)
         {
-            Name = categoryRequest.Name
         };
+
+        if (!category.IsValid)
+            return Results.BadRequest(category.Notifications);
+
         context.Categories.Add(category);
         context.SaveChanges();
 
